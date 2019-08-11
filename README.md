3c1297a6c13837e141c630df22a6d54b2a0d2a8f
taekyunKim <tuhbm2@gmail.com>
2019-08-11 15:42:46 +0900

    [MOD]rxjs 퀵스타트 브랜치 설계

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # RxJS 퀵스타트
 
 예제파일
+
+## branch
+- master
+> 기본
+- feature/autoSearchUi01
+> 자동 검색 UI 첫번째(git 사용자검색)
+- feature/autoSearchUi02
+> 자동 검색 UI 두번째(git 사용자검색)
+- feature/autoSearchUi03
+> 자동 검색 UI 세번째(git 사용자검색)
+- feature/carousel01
+> 캐러셀 UI 만들기
--- src/socketHotObservable.js
+++
@@ -1,7 +0,0 @@
-const { Observable } = rxjs;
-const socket = new WebSocket("ws://someUrl");
-const hotWebsocket$ = new Observable(function subscribe(observer) {
-   const handler = e => observer.next(e);
-   socket.addEventListener("message", handler);
-   return socket.removeEventListener("message", handler);
-});
