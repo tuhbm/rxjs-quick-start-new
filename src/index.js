baddb0a6dada809916731513d22dee7332c8d187
taekyunKim <tuhbm2@gmail.com>
2019-08-13 22:56:57 +0900

    [MOD]코드 개선

--- src/index.js
+++ src/index.js
@@ -16,31 +16,32 @@ $canvas.height = window.innerHeight/1.5;
 $canvas.style.border = '1px solid #000';
 $canvas.style.cursor = 'crosshair';
 
-function toPos(obs$) {
-    return obs$
-        .pipe(
-            map(v => v),
-            // tap(event => console.log(event))
-        );
-}
+// function toPos(obs$) {
+//     return obs$
+//         .pipe(
+//             map(v => v),
+//             // tap(event => console.log(event))
+//         );
+// }
 
-function drawLine(position, updatePositon) {
+function drawLine(position) {
     const {positionX, positionY} = position;
-    
+
     ctx.lineTo(positionX, positionY);
     ctx.stroke();
 
     // [lastX, lastY] = [positionX, positionY];
 }
 
-const start$ = fromEvent($canvas, EVENTS.start).pipe(toPos);
-const move$ = fromEvent($canvas, EVENTS.move).pipe(toPos);
-const end$ = fromEvent($canvas, EVENTS.end).pipe(toPos);
-const size$ = fromEvent(window, 'resize')
-    .pipe(
-        startWith(0),
-        map(event => $canvas.clientWidth)
-    );
+const start$ = fromEvent($canvas, EVENTS.start);
+const move$ = fromEvent($canvas, EVENTS.move);
+const end$ = fromEvent($canvas, EVENTS.end);
+const color$ = fromEvent($color);
+// const size$ = fromEvent(window, 'resize')
+//     .pipe(
+//         startWith(0),
+//         map(event => $canvas.clientWidth)
+//     );
 const clear$ = fromEvent($clear, 'click');
 
 // end$.subscribe(e => console.log('end$', e));
@@ -57,59 +58,62 @@ const drag$ = start$
         /**
          * drag$이 두번씩 호출됨 => share 오퍼레이터 사용
          * */
-        share(),
-        mergeAll(position => position)
+        // share(),
+        // mergeAll(position => position)
     );
 
-const drop$ = drag$
-    .pipe(
-        // map(drag => end$.pipe(take(1)))
-        /**
-         * first 오퍼레이터를 사용하면 자동 구독해제
-         * */
+// const drop$ = drag$
+//     .pipe(
+//         // map(drag => end$.pipe(take(1)))
+//         /**
+//          * first 오퍼레이터를 사용하면 자동 구독해제
+//          * */
+//
+//         // map(drag => end$.pipe(first())),
+//         // mergeAll()
+//         /**
+//          * map + mergeAll = mergeMap
+//          * mergeMap + (자동 구독해제) = switchMap
+//          */
+//         // tap(v => console.log('drog$', v)),
+//         switchMap(drag => {
+//             return end$.pipe(
+//                 /**
+//                  * drag는 drag$가 전달하는 start$와 move$의 위치 값의 거리
+//                  * */
+//                 map(event => drag),
+//                 first()
+//             )
+//         }),
+//         withLatestFrom((drag) => {
+//             return drag
+//         })
+//     );
 
-        // map(drag => end$.pipe(first())),
-        // mergeAll()
-        /**
-         * map + mergeAll = mergeMap
-         * mergeMap + (자동 구독해제) = switchMap
-         */
-        // tap(v => console.log('drog$', v)),
-        switchMap(drag => {
-            return end$.pipe(
-                /**
-                 * drag는 drag$가 전달하는 start$와 move$의 위치 값의 거리
-                 * */
-                map(event => drag),
-                first()
-            )
-        }),
-        withLatestFrom(size$, (drag) => {
-            return drag
-        })
-    );
-
-const drawTool$ = merge(drag$, drop$)
+const drawTool$ = drag$
     .pipe(
         map((store) => {
             const updateStore = {
                 positionX: store[0],
                 positionY : store[1]
             };
-            console.log('updateStore', updateStore);
+            return updateStore;
+        }),
+        tap((store) => {
             ctx.strokeStyle = $color.value;
             ctx.lineWidth = $size.value;
             ctx.lineJoin = 'round';
             ctx.lineCap = 'square';
-            
-            return updateStore;
+
+            ctx.beginPath();
+            ctx.moveTo(store.positionX, store.positionY);
+            return store
         })
     );
 
 drawTool$.subscribe(store => {
     // console.log('캐러셀 데이터', store);
-    ctx.beginPath();
-    ctx.moveTo(store.positionX, store.positionY);
+
     // console.log('store', store);
     drawLine({positionX: store.positionX, positionY: store.positionY})
 });
